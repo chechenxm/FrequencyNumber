3664ecf3c28c72c05a80285568498dc207c5c133
chechenxm <chenche741@gmail.com>
2017-04-24 21:15:23 +0800

    split String to words Array

--- fn-spec.js
+++ fn-spec.js
@@ -1,26 +1,27 @@
 
 var splitString = require('./fn.js')
 describe('split String to words', function(){
-        var a =['it','will'];
-        it('test1', function(){
-                expect(splitString('')).toEqual(null);
+        var a = ['it','will'];
+        it('return null given null', function(){
+                expect(splitString('')).toEqual('');
             }
         );
-        it('test1', function(){
-                expect(splitString('it   ')).toEqual(['it']);
+        it('return one word given one word ', function(){
+                expect(splitString('it')).toEqual(['it']);
             }
         );
-        it('test1', function(){
-                expect(splitString('it    will')).toEqual(a);
+        it('return two word given two word', function(){
+                expect(splitString('it will')).toEqual(['it','will']);
             }
         );
-        it('test1', function(){
-                expect(splitString('   it    will')).toEqual(a);
+        it('return three word given three word ', function(){
+                expect(splitString('it will get')).toEqual(['it', 'will', 'get']);
             }
         );
-    it('test1', function(){
-            expect(splitString('   it    will   ')).toEqual(a);
+    it('return two word given two word with some blanks', function(){
+            expect(splitString('   it  will  ')).toEqual(['it','will']);
         }
     );
     }
 );
+
--- fn.js
+++ fn.js
@@ -3,9 +3,14 @@
  */
 
 function splitString(String) {
+    if(String === '')
+        return '';
     let i;
+    String = String.replace(/(^\s*)|(\s*$)/g,'');
     var wordsString = new Array();
     wordsString = String.split(/\s+/);
     return wordsString;
 }
 module.exports = splitString;
+
+

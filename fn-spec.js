b9204493b2d944df741b67ba1c89d32cde1b92ef
chechenxm <chenche741@gmail.com>
2017-05-18 16:38:44 +0800

    count the frequency of words

--- fn-spec.js
+++ fn-spec.js
@@ -1,27 +1,42 @@
 
-var splitString = require('./fn.js')
+var frequencyNumber = require('./fn.js')
 describe('split String to words', function(){
-        var a = ['it','will'];
+
         it('return null given null', function(){
-                expect(splitString('')).toEqual('');
-            }
-        );
+            expect(frequencyNumber.splitString('')).toEqual('');
+        });
+
         it('return one word given one word ', function(){
-                expect(splitString('it')).toEqual(['it']);
-            }
-        );
+            expect(frequencyNumber.splitString('it')).toEqual(['it']);
+        });
+
         it('return two word given two word', function(){
-                expect(splitString('it will')).toEqual(['it','will']);
-            }
-        );
+            expect(frequencyNumber.splitString('it will')).toEqual(['it','will']);
+        });
+
         it('return three word given three word ', function(){
-                expect(splitString('it will get')).toEqual(['it', 'will', 'get']);
-            }
-        );
-    it('return two word given two word with some blanks', function(){
-            expect(splitString('   it  will  ')).toEqual(['it','will']);
-        }
-    );
+            expect(frequencyNumber.splitString('it will get')).toEqual(['it', 'will', 'get']);
+        });
+
+        it('return two word given two word with some blanks', function(){
+            expect(frequencyNumber.splitString('   it  will  ')).toEqual(['it','will']);
+        });
     }
 );
+
+describe('count the Frequency of words',function () {
+
+    it ('return one word and its frequency given one word', function () {
+        expect(frequencyNumber.countFreqy(['it'])).toEqual("it 1");
+    });
+
+  /*  it ('return two word and their own frequency given two different word', function () {
+        expect(frequencyNumber.countFreqy(['it','is'])).toEqual("it 1\r\nis 1");
+    });*/
+
+    it ('return one word and their own frequency given two same word', function () {
+        expect(frequencyNumber.countFreqy(['it','it'])).toEqual("it 2");
+    });
+
+});
 
--- fn.js
+++ fn.js
@@ -1,16 +1,63 @@
-/**
- * Created by ASUS on 2017/4/16.
- */
+
 
 function splitString(String) {
     if(String === '')
         return '';
     let i;
     String = String.replace(/(^\s*)|(\s*$)/g,'');
-    var wordsString = new Array();
+    let wordsString = new Array();
     wordsString = String.split(/\s+/);
     return wordsString;
 }
-module.exports = splitString;
 
+function countFreqy(wordsString){
+    var count = new Array;
+    var words = new Array;
+    var wordsFrequency = new Array;
+    var cou;
+    var wor = {};
+    var index;
+    for(var i = 0, len = wordsString.length; i < len; i ++ ){
+       /* if( (index = words.indexOf(wordsString[i])) == -1){
+            words.push(wordsString[i]);
+            wordsFrequency.push({word: wordsString[i],count:1});
+        }else{
+            wordsFrequency[index].count ++;
+        }*/
+
+        if(wor[wordsString[i]] == undefined ){
+            wordsFrequency.push({word: wordsString[i],count:1});
+            wor[wordsString[i]] = wordsFrequency.length - 1;
+
+        }else {
+
+            wordsFrequency[wor[wordsString[i]]].count ++;
+        }
+    }
+
+    return format(wordsFrequency);
+}
+
+function sort(){
+
+}
+
+function  format( wordsFrequency) {
+    var result = ''
+    for(var i = 0,len =  wordsFrequency.length; i < len; i ++){
+        if(i > 0){
+            result += '\r\n';
+        }
+        result = result +  wordsFrequency[i].word + ' ' +  wordsFrequency[i].count;
+    }
+    return result;
+}
+
+
+
+module.exports = {
+    splitString : splitString,
+    countFreqy : countFreqy,
+    t1 : t1
+}
 
